2b6a98e7f9967a70fef66fdc570201d6980b49f4
dayraquin <71858583+dayraquin@users.noreply.github.com>
2020-11-08 15:50:00 -0700

    Update lab7.py

--- lab7.py
+++ lab7.py
@@ -7,36 +7,37 @@ def new_contact_store():
 
 
 def add_new_contact(contacts, first_name, last_name, email, phone_number, birthday):
-    contacts[first_name, last_name] = email, phone_number, birthday
+    contacts[""] = first_name, last_name, email, phone_number, birthday
+
 
 
 def has_contact(contacts, first_name, last_name):
-    contacts.get(first_name, last_name)
 
 
 def get_contact_string(contacts, first_name, last_name):
-    contacts.get(first_name, last_name)
 
 
 def remove_contact(contacts, first_name, last_name):
-    contacts.remove(first_name, last_name)
+   contacts.remove(first_name)
+   contacts.remove(last_name)
 
 
 def update_contact_first_name(contacts, first_name, last_name, new_field_value):
-    contacts.update(first_name)
+   contacts.update(first_name)
+
 
 
 def update_contact_last_name(contacts, first_name, last_name, new_field_value):
-    contacts.update(last_name)
-
+   contacts.update(last_name)
 
 def update_contact_email(contacts, first_name, last_name, new_field_value):
-    contacts.update(email)
-
+   email = new_field_value
+   contacts.update(email)
 
 def update_contact_phone_number(contacts, first_name, last_name, new_field_value):
-    contacts.update(phone_number)
-
+   phone_number = new_field_value
+   contacts.update(phone_number)
 
 def update_contact_birthday(contacts, first_name, last_name, new_field_value):
-    contacts.update(contact_birthday)
+   birthday = new_field_value
+   contacts.update(birthday)
