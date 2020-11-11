c24fa56274599c8923b641f73346f2d0b0436880
Kendra <0kendraerin0@gmail.com>
2020-11-11 15:58:10 -0700

    Fixed first name function

--- lab7.py
+++ lab7.py
@@ -8,11 +8,11 @@ def new_contact_store():
 
 def add_new_contact(contacts, first_name, last_name, email, phone_number, birthday):
     key = first_name + last_name
-    contacts[key] = { "First" : first_name,
-                      "Last" : last_name,
+    contacts[key] = { "First Name" : first_name,
+                      "Last Name" : last_name,
                       "Email" : email,
-                      "Number" : phone_number,
-                      "Birthdate" : birthday
+                      "Phone Number" : phone_number,
+                      "Birthday" : birthday
                       }
     return contacts
 
@@ -38,27 +38,34 @@ def remove_contact(contacts, first_name, last_name):
 
 def update_contact_first_name(contacts, first_name, last_name, new_field_value):
     key = first_name + last_name
-    contacts[key]["First"] = new_field_value
+    new_key = new_field_value + last_name
+    contacts[key]["First Name"] = new_field_value
+    contacts[new_key] = contacts[key]
+
     return contacts
 
 
 def update_contact_last_name(contacts, first_name, last_name, new_field_value):
-   last_name = new_field_value
-   updated = contacts.update(last_name)
-   return updated
+    key = first_name + last_name
+
+    updated = contacts.update(last_name)
+    return updated
 
 def update_contact_email(contacts, first_name, last_name, new_field_value):
-   email = new_field_value
-   updated = contacts.update(email)
-   return updated
+    key = first_name + last_name
+
+    updated = contacts.update(email)
+    return updated
 
 def update_contact_phone_number(contacts, first_name, last_name, new_field_value):
-   phone_number = new_field_value
-   updated = contacts.update(phone_number)
-   return updated
+    key = first_name + last_name
+
+    phone_number = new_field_value
+    updated = contacts.update(phone_number)
+    return updated
 
 def update_contact_birthday(contacts, first_name, last_name, new_field_value):
-   birthday = new_field_value
-   updated = contacts.update(birthday)
-   return updated
+    key = first_name + last_name
 
+    updated = contacts.update(birthday)
+    return updated
