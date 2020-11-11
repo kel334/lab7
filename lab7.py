5b3cfecca4e1a961478a74b0ee37d414d294f429
dayraquin <71858583+dayraquin@users.noreply.github.com>
2020-11-11 14:13:37 -0700

    Update lab7.py

--- lab7.py
+++ lab7.py
@@ -28,20 +28,23 @@ def remove_contact(contacts, first_name, last_name):
 def update_contact_first_name(contacts, first_name, last_name, new_field_value):
    first_name = new_field_value
    contacts.update(first_name)
-
+   return first_name
 
 def update_contact_last_name(contacts, first_name, last_name, new_field_value):
    last_name = new_field_value
    contacts.update(last_name)
+   return last_name
 
 def update_contact_email(contacts, first_name, last_name, new_field_value):
    email = new_field_value
    contacts.update(email)
+   return email
 
 def update_contact_phone_number(contacts, first_name, last_name, new_field_value):
    phone_number = new_field_value
    contacts.update(phone_number)
-
+   return phone_number
 def update_contact_birthday(contacts, first_name, last_name, new_field_value):
    birthday = new_field_value
    contacts.update(birthday)
+   return birthday
